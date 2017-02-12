f1cc881b683c8c40b24376383aab68630e9b5138
Asif <asifadam93@Asifs-Mac.local>
2017-02-12 15:51:36 +0100

    testXMLFile added

--- EasyXMLParserTests/UtilisationXMLParserTest.swift
+++ EasyXMLParserTests/UtilisationXMLParserTest.swift
@@ -13,10 +13,10 @@ import XCTest
 class UtilisationXMLParserTest: XCTestCase {
     
     /****************************************************************
-        Simple user case test to find the same blog name
+        Simple user test case to find the same blog name
      ****************************************************************/
     
-    func testUtilisation() {
+    func testUrl() {
         
         if let url = URL.init(string: "https://korben.info/feed"){
             
@@ -36,4 +36,55 @@ class UtilisationXMLParserTest: XCTestCase {
         
     }
     
+    /****************************************************************
+        Simple user test case to parse xml file
+     ****************************************************************/
+    
+    func testXMLFile() {
+        
+        if let utilisateursFichier = Bundle.main.path(forResource: "utilisateurs", ofType: "xml") {
+            
+            if let utilisateursString = try? String(contentsOfFile: utilisateursFichier) {
+                
+                if let utilisateurData = utilisateursString.data(using: .utf8) {
+                    
+                    let parser = EasyXMLParser(withData: utilisateurData)
+                    
+                    let items = parser.parse()
+                    
+                    var item = items["utilisateurs"]["utilisateur"].get()
+                    
+                    
+                }
+            }
+        }
+    }
+    
+    private func affichage(elements: [EasyXMLElement]) {
+        
+        for element in elements {
+            
+            if var tempoValue = element.value as? String {
+                
+                let sizeTempoValue = tempoValue.distance(from: tempoValue.startIndex, to: tempoValue.endIndex)
+                
+                let tempoValue2 = tempoValue.substring(to: tempoValue.index(    tempoValue.startIndex, offsetBy: ((sizeTempoValue  > 80) ? 80 : sizeTempoValue)))
+                
+                if (tempoValue.characters.count > tempoValue2.characters.count) {
+                    
+                    print ("\(element.name) : \(tempoValue2) ...")
+                    
+                    XCTAssertEqual(tempoValue2,"Lenore")
+
+                } else {
+                    
+                    print ("\(element.name) : \(tempoValue)")
+                    
+                    XCTAssertEqual(tempoValue2,"Lenore")
+                }
+            } else {
+                print ("\(element.name) : VIDE")
+            }
+        }
+    }
 }
